4699f77bb12bc672af3247521b2fef68265a74da
jeshu2002 <71264878+jeshu2002@users.noreply.github.com>
2024-03-06 01:16:49 +0530

    name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
             href="/models"
             className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           >
-            Serach Models{" "}
+            Search Models{" "}
             <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
           </a>
         </motion.div>
